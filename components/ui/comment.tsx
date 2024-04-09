ec0100919c73ac4f752b2ac0cae494deb1ab0df7
James E <4589250+Naught0@users.noreply.github.com>
2024-04-09 03:37:29 -0400

    Use new comment/thread models

--- components/ui/comment.tsx
+++ components/ui/comment.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { Timestamp } from "./timestamp";
+import { RiHashtag } from "react-icons/ri";
 
-const indentLevels = ["ml-0", "ml-4", "ml-8", "ml-12", "ml-16", "ml-20"];
+const indentLevels = ["ml-0", "ml-6", "ml-12", "ml-18", "ml-24", "ml-28"];
+
 const indentColorsClassName = [
   "border-slate-300",
   "border-slate-400",
@@ -11,30 +13,30 @@ const indentColorsClassName = [
   "border-zinc-500",
   "border-zinc-600",
 ];
-
 export const HNComment = (props: HNComment & { indentLevel?: number }) => {
   return (
     <article
-      className={`flex flex-col justify-between gap-2 border-l-2 border-solid ${
+      id={`${props.id}`}
+      className={`flex flex-col justify-between gap-2 ${
         indentColorsClassName?.[props.indentLevel ?? 0] ??
         indentColorsClassName[-1]
-      } py-1 pl-3 ${
-        indentLevels?.[props.indentLevel ?? 0] ?? indentLevels[-1]
+      } py-2 pl-3 ${
+        indentLevels?.[props.indentLevel ?? 0] ?? indentLevels[0]
       } mb-2 min-w-0 max-w-fit`}
     >
       <div className="flex flex-row items-center gap-1 text-sm lg:text-base">
         <Link href={`/user/${props.by}`} className="text-muted-foreground">
           {props.by}
         </Link>
-        <Timestamp
-          time={props.time}
-          timeAgo={new Date(props.time * 1000).toLocaleString()}
-        />
+        <Timestamp time={props.time} />
+        <Link href={`#${props.id}`}>
+          <RiHashtag />
+        </Link>
       </div>
       {props.text && (
         <div
           dangerouslySetInnerHTML={{ __html: props.text }}
-          className="content"
+          className="prose prose-sm md:prose-base prose-slate dark:prose-invert"
         />
       )}
     </article>
--- components/ui/thread.tsx
+++ components/ui/thread.tsx
@@ -1,32 +1,4 @@
-import { getThreadComments } from "~/app/hackernews-api";
-import { Comment, HNComment } from "./comment";
-
-export const Thread = async (
-  props: TComment & { maxDepth?: number; indentLevel?: number },
-) => {
-  const comments = await getThreadComments(props.id);
-
-  return (
-    <div>
-      <Comment {...props} />
-      {comments.children.map((c) => {
-        return c.children ? (
-          <Thread
-            key={c.id}
-            {...c}
-            indentLevel={(props.indentLevel ?? 0) + 1}
-          />
-        ) : (
-          <Comment
-            key={c.id}
-            {...c}
-            indentLevel={(props.indentLevel ?? 0) + 1}
-          />
-        );
-      })}
-    </div>
-  );
-};
+import { HNComment } from "./comment";
 
 export const HNThreadComponent = async (
   props: HNComment & { maxDepth?: number; indentLevel?: number },
