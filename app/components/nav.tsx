18d7027836d83c8aed568f2d83222f4139cafdfe
James E <4589250+Naught0@users.noreply.github.com>
2024-04-15 19:51:41 -0400

    Update header

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
 import { ModeToggle } from "~/components/ui/theme-toggle";
 
 export const Nav = () => {
   return (
     <div className="flex flex-row justify-center p-6">
-      <div className="w-full max-w-screen-lg flex-grow">
-        <ModeToggle />
+      <div className="flex w-full max-w-screen-lg flex-grow flex-row items-center justify-between gap-3 lg:gap-6">
+        <div className="flex">
+          <Link href="/">slackernews</Link>
+        </div>
+        <div className="flex">
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
