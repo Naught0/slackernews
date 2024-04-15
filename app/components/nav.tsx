42c94f999b63163dcd4322e1b9c0675fcb923beb
James E <4589250+Naught0@users.noreply.github.com>
2024-04-15 19:48:52 -0400

    Center header

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,8 +2,10 @@ import { ModeToggle } from "~/components/ui/theme-toggle";
 
 export const Nav = () => {
   return (
-    <div className="flex flex-row p-6">
-      <ModeToggle />
+    <div className="flex flex-row justify-center p-6">
+      <div className="w-full max-w-screen-lg flex-grow">
+        <ModeToggle />
+      </div>
     </div>
   );
 };
