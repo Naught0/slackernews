aeb5c3283eb2cf18d16168576a5007ff18cf5a46
James E <4589250+Naught0@users.noreply.github.com>
2024-04-16 01:13:59 -0400

    Show pages in logo

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,12 +1,19 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 import { ModeToggle } from "~/components/ui/theme-toggle";
 
 export const Nav = () => {
+  const path = usePathname();
+  const params = useSearchParams();
+  const page = params.get("page") ?? "1";
   return (
     <div className="flex flex-row justify-center p-6">
       <div className="flex w-full max-w-screen-lg flex-grow flex-row items-center justify-between gap-3 lg:gap-6">
         <div className="flex">
-          <Link href="/">/slacker_news</Link>
+          <Link href="/" className="font-mono">
+            /slacker_news{path.length > 1 && path}?page={page}
+          </Link>
         </div>
         <div className="flex">
           <ModeToggle />
