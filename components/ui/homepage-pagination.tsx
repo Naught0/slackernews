8f1e5ab0e83e97d6f50093eed9cacbfdbfe55d8e
James E <4589250+Naught0@users.noreply.github.com>
2024-04-16 00:21:02 -0400

    Set pages properly

--- components/ui/homepage-pagination.tsx
+++ components/ui/homepage-pagination.tsx
@@ -11,13 +11,13 @@ import {
 export function HomepagePagination({
   searchParams,
 }: {
-  searchParams: Record<string, string | undefined>;
+  searchParams?: Record<string, string | undefined>;
 }) {
   const page = parseInt(searchParams?.["page"] ?? "1");
   useEffect(
     function removePageParam() {
       if (page === 1) {
-        history.replaceState({}, "", "/");
+        history.replaceState({}, "", window.location.pathname);
       }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -27,10 +27,10 @@ export function HomepagePagination({
     <Pagination>
       <PaginationContent>
         <PaginationItem>
-          <PaginationPrevious href={`/?page=${page - 1 > 0 ? page - 1 : 1}`} />
+          <PaginationPrevious href={`?page=${page - 1 > 0 ? page - 1 : 1}`} />
         </PaginationItem>
         <PaginationItem>
-          <PaginationNext href={`/?page=${page + 1}`} />
+          <PaginationNext href={`?page=${page + 1}`} />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
