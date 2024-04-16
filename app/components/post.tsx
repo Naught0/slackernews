e1891459781083845521dea215621f2bc194e18a
James E <4589250+Naught0@users.noreply.github.com>
2024-04-15 21:56:04 -0400

    Sanitize html

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -12,35 +12,35 @@ export const Post = ({
     <article
       {...props}
       className={cn(
-        "flex flex-col gap-3 rounded-md border border-solid border-slate-300 bg-primary px-6 py-4 transition-colors hover:border-slate-400 dark:border-slate-700 hover:dark:border-slate-600",
+        "flex flex-col gap-3 border-b border-solid border-slate-300 p-3 dark:border-slate-700",
         props.className,
       )}
     >
-      <div className="flex flex-row gap-3">
-        <span className="align-middle text-muted-foreground">
-          <TriangleUpIcon className="inline size-6" />
-          {story.score}
-        </span>{" "}
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-col gap-1">
-            {"url" in story ? (
-              <Link href={story.url} className="w-fit text-lg lg:text-xl">
-                {story.title}
-                <p className="ml-1 break-all text-xs text-muted-foreground lg:text-sm">
-                  {new URL(story.url).hostname}
-                </p>
-              </Link>
-            ) : (
-              <p className="w-fit text-lg lg:text-xl">{story.title}</p>
-            )}
-            <Link
-              href={`/user/${story.by}`}
-              className="flex w-fit items-center gap-1 text-sm text-slate-500 dark:text-muted-foreground md:text-base"
-            >
-              <PersonIcon />
-              {story.by}
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-1">
+          {"url" in story ? (
+            <Link href={story.url} className="w-fit text-lg lg:text-xl">
+              {story.title}
+              <p className="ml-1 break-all text-xs text-muted-foreground lg:text-sm">
+                {new URL(story.url).hostname}
+              </p>
             </Link>
-          </div>
+          ) : (
+            <p className="w-fit text-lg lg:text-xl">{story.title}</p>
+          )}
+          <Link
+            href={`/user/${story.by}`}
+            className="flex w-fit items-center gap-1 text-sm text-slate-500 dark:text-muted-foreground md:text-base"
+          >
+            <PersonIcon />
+            {story.by}
+          </Link>
+        </div>
+        <div className="flex flex-row items-center gap-3">
+          <span className="align-middle text-sm text-secondary-foreground lg:text-base">
+            <TriangleUpIcon className="inline size-6" />
+            {story.score}
+          </span>
           {"descendants" in story && (
             <PostActions comments={story.descendants} postId={story.id} />
           )}
