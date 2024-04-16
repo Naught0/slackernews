ef8fd4e55c45933b0d00c85975732773eb04c00e
James E <4589250+Naught0@users.noreply.github.com>
2024-04-16 01:28:40 -0400

    Fix header on non homepage

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -6,13 +6,13 @@ import { ModeToggle } from "~/components/ui/theme-toggle";
 export const Nav = () => {
   const path = usePathname();
   const params = useSearchParams();
-  const page = params.get("page") ?? "1";
   return (
     <div className="flex flex-row justify-center p-6">
       <div className="flex w-full max-w-screen-lg flex-grow flex-row items-center justify-between gap-3 lg:gap-6">
         <div className="flex">
           <Link href="/" className="font-mono">
-            /slacker_news{path.length > 1 && path}?page={page}
+            /slacker_news{path.length > 1 && path}
+            {params.size > 0 && "?" + new URLSearchParams(params).toString()}
           </Link>
         </div>
         <div className="flex">
