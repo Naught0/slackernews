4b94e23cf7ca0d8502b01096206ca52696192440
James E <4589250+Naught0@users.noreply.github.com>
2024-04-15 22:50:08 -0400

    Allow wrapping

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -16,7 +16,7 @@ export default async function Page({
   return (
     <div className="flex w-full max-w-screen-lg flex-col gap-3">
       <div className="flex flex-col flex-wrap gap-3">
-        <div className="flex flex-1 flex-row gap-3">
+        <div className="flex flex-1 flex-row flex-wrap gap-3">
           <BackHomeButton />
           <Post story={story} className="flex-grow" />
         </div>
