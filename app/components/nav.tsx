1a6df129d9be1e19e35ed54f5da84e4530b4d9a2
James E <4589250+Naught0@users.noreply.github.com>
2024-04-16 17:14:59 -0400

    Make nav client component

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,12 +1,19 @@
+"use client";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 import { ModeToggle } from "~/components/ui/theme-toggle";
-import { ActiveLogo } from "./active-logo";
 
 export const Nav = () => {
+  const path = usePathname();
+  const params = useSearchParams();
   return (
     <div className="flex flex-row justify-center p-6">
       <div className="flex w-full max-w-screen-lg flex-grow flex-row items-center justify-between gap-3 lg:gap-6">
         <div className="flex">
-          <ActiveLogo />
+          <Link href="/" className="font-mono">
+            /slacker_news{path.length > 1 && path}
+            {params.size > 0 && "?" + new URLSearchParams(params).toString()}
+          </Link>
         </div>
         <div className="flex">
           <ModeToggle />
