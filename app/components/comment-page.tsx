11c93344d25587f2975cc3a59eda9c2716fb62bf
James E <4589250+Naught0@users.noreply.github.com>
2024-04-18 00:43:48 -0400

    Break out comment page

---
+++ app/components/comment-page.tsx
@@ -0,0 +1,59 @@
+import { GoArrowLeft, GoArrowUp } from "react-icons/go";
+import { getItemById } from "../hackernews-api";
+import { getItem } from "../hackernews-api/hnpwa";
+import { HNComment } from "../post/components/comment";
+import { HNThreadComponent } from "../post/components/thread";
+import Link from "next/link";
+
+export async function CommentPage({
+  commentId,
+  postId,
+}: {
+  commentId: string;
+  postId?: string;
+}) {
+  const comment = await getItem(commentId);
+  const rawComment = await getItemById<HNComment>(commentId);
+  const contextLink = () => {
+    if (!rawComment.parent) return null;
+
+    if (rawComment.parent.toString() === postId) {
+      return (
+        <Link href={`/post/${postId}`} className="gap-1">
+          <GoArrowLeft className="mr-1 inline" />
+          <span className="underline">Back to post</span>
+        </Link>
+      );
+    }
+    if (postId && rawComment.parent) {
+      return (
+        <Link href={`/post/${postId}/comment/${rawComment.parent}`}>
+          <GoArrowUp className="mr-1 inline" />
+          <span className="underline">See more context</span>
+        </Link>
+      );
+    }
+
+    return (
+      <Link href={`/comment/${rawComment.parent}`} className="underline">
+        <GoArrowUp className="mr-1 inline" /> See parent comment
+      </Link>
+    );
+  };
+  return (
+    <>
+      {rawComment.parent && contextLink()}
+      {comment.type === "comment" && (
+        <HNComment postId={postId} op={comment.user} {...comment} />
+      )}
+      {comment?.comments?.map((c) => (
+        <HNThreadComponent
+          key={c.id}
+          op={comment.user}
+          {...c}
+          postId={postId}
+        />
+      ))}
+    </>
+  );
+}
--- app/post/[id]/comment/[commentId]/page.tsx
+++ app/post/[id]/comment/[commentId]/page.tsx
@@ -1,45 +1,9 @@
-import Link from "next/link";
-import { GoArrowLeft, GoArrowUp } from "react-icons/go";
-import { getItemById } from "~/app/hackernews-api";
-import { getItem } from "~/app/hackernews-api/hnpwa";
-import { HNComment } from "~/app/post/components/comment";
-import { HNThreadComponent } from "~/app/post/components/thread";
+import { CommentPage } from "~/app/components/comment-page";
 
 export default async function Page({
   params: { commentId, id },
 }: {
   params: { id: string; commentId: string };
 }) {
-  const comment = await getItem(commentId);
-  const rawComment = await getItemById<HNComment>(commentId);
-  const contextLink = () => {
-    if (!rawComment.parent) return null;
-
-    if (rawComment.parent.toString() === id) {
-      return (
-        <Link href={`/post/${id}`} className="gap-1">
-          <GoArrowLeft className="mr-1 inline" />
-          <span className="underline">Back to post</span>
-        </Link>
-      );
-    } else {
-      return (
-        <Link href={`/post/${id}/comment/${rawComment.parent}`}>
-          <GoArrowUp className="mr-1 inline" />
-          <span className="underline">See more context</span>
-        </Link>
-      );
-    }
-  };
-  return (
-    <>
-      {rawComment.parent && contextLink()}
-      {comment.type === "comment" && (
-        <HNComment postId={id} op={comment.user} {...comment} />
-      )}
-      {comment?.comments?.map((c) => (
-        <HNThreadComponent key={c.id} op={comment.user} {...c} postId={id} />
-      ))}
-    </>
-  );
+  <CommentPage postId={id} commentId={commentId} />;
 }
