2b8aa3f75e6030c45cf0d2a85595ede8acdb16a7
James E <4589250+Naught0@users.noreply.github.com>
2024-05-08 23:29:24 -0400

    Handle invalid routes

--- app/[homepageType]/page.tsx
+++ app/[homepageType]/page.tsx
@@ -1,9 +1,20 @@
+import { redirect } from "next/navigation";
 import Homepage from "../components/Homepage";
 
 export default function Home(props: {
   searchParams: Record<string, string | undefined>;
   params: { homepageType: HNPWAFeedType };
 }) {
+  if (
+    !(
+      ["show", "news", "ask", "jobs", "newest", "best"] as (
+        | HNPWAFeedType
+        | "best"
+      )[]
+    ).includes(props.params.homepageType)
+  ) {
+    return redirect("/");
+  }
   return (
     <Homepage
       searchParams={props?.searchParams}
