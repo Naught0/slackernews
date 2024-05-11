a6fd7acab040ade4a746672b294ffe38fc42d76e
James E <4589250+Naught0@users.noreply.github.com>
2024-05-11 04:23:57 -0400

    Redirect top comments to post

--- app/comment/[id]/page.tsx
+++ app/comment/[id]/page.tsx
@@ -7,5 +7,8 @@ export default async function Page({
   params: { id: string };
 }) {
   const post = await getCommentPost(id);
+  if (post.id.toString() === id) {
+    return permanentRedirect(`/post/${post.id}`, RedirectType.replace);
+  }
   permanentRedirect(`/post/${post.id}/comment/${id}`, RedirectType.replace);
 }
