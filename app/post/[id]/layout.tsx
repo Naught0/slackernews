4e3b8066b7cbb2f987f3ea7bcad084c0be284ee4
James E <4589250+Naught0@users.noreply.github.com>
2024-07-01 22:42:34 -0400

    Fix spacing

--- app/post/[id]/layout.tsx
+++ app/post/[id]/layout.tsx
@@ -15,14 +15,13 @@ export default async function Layout({
   if (!story) notFound();
 
   return (
-    <div className="flex w-full max-w-screen-lg flex-col gap-3">
+    <div className="flex w-full max-w-screen-lg flex-col gap-6">
       <div className="flex flex-col flex-wrap gap-3">
         <div className="border-color flex flex-1 flex-row flex-wrap gap-3 border-b pb-3">
           <BackHomeButton />
           <Post story={story} className="flex-grow" showHnLink />
         </div>
       </div>
-      <br />
       <div className="comments flex flex-col gap-3">{children}</div>
     </div>
   );
