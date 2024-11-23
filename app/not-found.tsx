291481152751ae419f383ed68d51a9613d8f909c
James E <4589250+Naught0@users.noreply.github.com>
2024-11-23 05:31:26 -0500

    Async function

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import { Separator } from "~/components/ui/separator";
 
-export default function Page() {
+export default async function Page() {
   return (
     <div className="flex max-w-screen-sm flex-grow flex-row items-center gap-3 lg:gap-8">
       <h1 className="text-6xl font-bold lg:text-9xl">404</h1>
