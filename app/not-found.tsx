a09c8d9fec3d4cf59746e6a89791706f3bb8ef78
James E <4589250+Naught0@users.noreply.github.com>
2024-11-23 04:57:49 -0500

    Add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import { Separator } from "~/components/ui/separator";
+
+export default function Page() {
+  return (
+    <div className="flex max-w-screen-sm flex-grow flex-row items-center gap-3 lg:gap-8">
+      <h1 className="text-6xl font-bold lg:text-9xl">404</h1>
+      <Separator orientation="vertical" className="h-16 lg:h-32" />
+      <p className="text-xl lg:text-3xl">Not found</p>
+    </div>
+  );
+}
