4008af02265e124018dbca8aa728f175375cc647
James E <4589250+Naught0@users.noreply.github.com>
2025-04-03 00:43:18 -0400

    Move fetch/render outside cached layout

--- app/post/[id]/layout.tsx
+++ app/post/[id]/layout.tsx
@@ -1,27 +1,7 @@
-import { notFound } from "next/navigation";
 import { ReactNode } from "react";
-import { Post } from "~/app/components/post";
-import { getItem } from "~/app/hackernews-api/hnpwa";
 
-export default async function Layout({
-  params,
-  children,
-}: {
-  params: Promise<{ id: string }>;
-  children: ReactNode;
-}) {
-  const { id } = await params;
-  const story = await getItem(id);
-  if (!story) notFound();
-
+export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex w-full max-w-screen-lg flex-col gap-6">
-      <div className="flex flex-col flex-wrap gap-3">
-        <div className="border-color flex flex-1 flex-row flex-wrap gap-3 border-b pb-3">
-          <Post story={story} className="flex-grow" showHnLink />
-        </div>
-      </div>
-      <div className="comments flex flex-col gap-3">{children}</div>
-    </div>
+    <div className="flex w-full max-w-screen-lg flex-col gap-6">{children}</div>
   );
 }
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import { AnchorButtons } from "../components/anchor-buttons";
 import { HNThreadComponent } from "../components/thread";
 import { getItem } from "~/app/hackernews-api/hnpwa";
+import { Post } from "~/app/components/post";
 
 export default async function Page({
   params,
@@ -13,16 +14,21 @@ export default async function Page({
   if (!thread) notFound();
 
   return (
-    <>
-      {thread.comments?.map((comment) => (
-        <HNThreadComponent
-          postId={id}
-          op={thread.user}
-          key={comment.id}
-          {...comment}
-        />
-      ))}
-      <AnchorButtons />
-    </>
+    <div className="flex flex-col flex-wrap gap-3">
+      <div className="border-color flex flex-1 flex-row flex-wrap gap-3 border-b pb-3">
+        <Post story={thread} className="flex-grow" showHnLink />
+      </div>
+      <div className="comments flex flex-col gap-3">
+        {thread.comments?.map((comment) => (
+          <HNThreadComponent
+            postId={id}
+            op={thread.user}
+            key={comment.id}
+            {...comment}
+          />
+        ))}
+        <AnchorButtons />
+      </div>
+    </div>
   );
 }
