ef05142ff7e8407b8e8fbcce0835d7334f6bfbec
James E <4589250+Naught0@users.noreply.github.com>
2026-01-04 21:43:52 -0500

    Genericize this component

--- app/post/components/anchor-buttons.tsx
+++ app/post/components/anchor-buttons.tsx
@@ -2,51 +2,21 @@
 import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 import { Button } from "~/components/ui/button";
 
-export function AnchorButtons() {
-  function next() {
-    const elem = findNextAnchor();
-    if (!elem) return;
-
-    elem.scrollIntoView({ block: "start", behavior: "smooth" });
-  }
-  function prev() {
-    const elem = findPrevAnchor();
-    if (!elem) return window.scrollTo({ top: 0 });
-
-    elem.scrollIntoView({ block: "start", behavior: "smooth" });
-  }
-
+export function AnchorButtons({
+  onNext,
+  onPrev,
+}: {
+  onNext: () => void;
+  onPrev: () => void;
+}) {
   return (
     <div className="fixed bottom-1/3 right-12 flex w-fit flex-col gap-1 opacity-45 hover:opacity-85 lg:right-24">
-      <Button onClick={prev} type="button" size={"icon"} variant={"outline"}>
+      <Button onClick={onPrev} type="button" size={"icon"} variant={"outline"}>
         <ArrowUpIcon />
       </Button>
-      <Button onClick={next} type="button" size="icon" variant="outline">
+      <Button onClick={onNext} type="button" size="icon" variant="outline">
         <ArrowDownIcon />
       </Button>
     </div>
   );
-}
-
-function findAnchors() {
-  const anchors = [...document.querySelectorAll(".anchor")].toSorted(
-    (a, b) => b.getBoundingClientRect().y - a.getBoundingClientRect().y,
-  );
-  return anchors;
-}
-
-function findPrevAnchor() {
-  return findAnchors().find((a) => a.getBoundingClientRect().y < -25);
-}
-
-function findNextAnchor() {
-  // Source: https://medium.com/@alan.nguyen2050/detect-scroll-reaches-the-bottom-acb315824214
-  const scrolledTo = window.scrollY + window.innerHeight;
-  const threshold = 150;
-  const cantScroll = document.body.scrollHeight - threshold <= scrolledTo;
-  if (cantScroll) return;
-
-  return findAnchors()
-    .toReversed()
-    .find((a) => a.getBoundingClientRect().y > 25);
 }
