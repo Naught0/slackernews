d60b05b3a3c1da843ae3dd9755fd681adbad6dd5
James E <4589250+Naught0@users.noreply.github.com>
2026-01-05 23:35:34 -0500

    Reduce width

--- app/post/[id]/layout.tsx
+++ app/post/[id]/layout.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex w-full max-w-screen-lg flex-col gap-6">{children}</div>
+    <div className="flex w-full max-w-screen-md flex-col gap-6">{children}</div>
   );
 }
