f13f7eb2aaed1011176d2aa1f58d2669beb6bd2a
James E <4589250+Naught0@users.noreply.github.com>
2026-01-05 21:59:21 -0500

    Simplify loading

--- app/post/[id]/loading.tsx
+++ app/post/[id]/loading.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import { Separator } from "~/components/ui/separator";
 import { Skeleton } from "~/components/ui/skeleton";
-import { randomChoice } from "~/lib/utils";
 
 const LoadingComment = (
   props: HNPWAItem & { op?: string | null; postId: string },
@@ -27,13 +26,7 @@ const LoadingComment = (
 
 function generateComment() {
   return {
-    user: randomChoice([
-      "billy",
-      "bingus_springleMcGee",
-      "another name",
-      "dr_Octagonapus_III_Esq.",
-      "bap",
-    ]),
+    user: "dr_Octagonapus_III_Esq.",
     postId: "0",
     comments: [],
     comments_count: 0,
@@ -47,7 +40,7 @@ function generateComment() {
     content:
       "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.".slice(
         0,
-        randomChoice([120, 300, 500, 800, 1000]),
+        100,
       ),
   };
 }
@@ -55,6 +48,8 @@ function generateComment() {
 export default function Loading() {
   return (
     <div className="flex flex-col gap-3">
+      <LoadingComment {...generateComment()} />
+      <Separator className="my-3" />
       {[...new Array(25).keys()].map((idx) => (
         <LoadingComment key={idx} {...generateComment()} />
       ))}
