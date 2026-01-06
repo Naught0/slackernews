5862c92a8fb20f5b95e9bcc6e59b4951d344d1f6
James E <4589250+Naught0@users.noreply.github.com>
2026-01-05 23:35:40 -0500

    Add back randomness to loaders

--- app/post/[id]/loading.tsx
+++ app/post/[id]/loading.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { Separator } from "~/components/ui/separator";
 import { Skeleton } from "~/components/ui/skeleton";
+import { randomChoice } from "~/lib/utils";
 
 const LoadingComment = (
   props: HNPWAItem & { op?: string | null; postId: string },
@@ -26,7 +28,13 @@ const LoadingComment = (
 
 function generateComment() {
   return {
-    user: "dr_Octagonapus_III_Esq.",
+    user: randomChoice([
+      "billy",
+      "bingus_springleMcGee",
+      "another name",
+      "dr_Octagonapus_III_Esq.",
+      "bap",
+    ]),
     postId: "0",
     comments: [],
     comments_count: 0,
@@ -40,7 +48,7 @@ function generateComment() {
     content:
       "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.".slice(
         0,
-        100,
+        randomChoice([120, 300, 500, 800, 1000]),
       ),
   };
 }
